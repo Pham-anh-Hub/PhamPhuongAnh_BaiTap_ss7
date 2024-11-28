85c4c3ba6e9baa61e844ee3d2cb3d1838fd062a2
Pham-anh-Hub <phamphuonganhcutee@gmail.com>
2024-11-28 13:13:21 +0700

    Update Bai2_ss7.c

--- Bai2_ss7.c
+++ Bai2_ss7.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 int main(){
 	int num[5];
-	int a,; 
+	 
 	for(int i=0; i<5; i++){
 		printf("Moi nhap vao so nguyen thu %d ",i+1);
 		scanf("%d",&num[i]);
-		count++; 
 	}for(int i=0; i<5; i++){
 		printf("\nnum[%d] = %d ",i, num[i]);
 	}
